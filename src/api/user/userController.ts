dba1615c5e523e755783197806aaa4366b18c27b
sandeep0009 <500sandeepks@gmail.com>
2025-03-06 18:57:12 +0530

    fix:fixed some deployement issues

--- src/api/user/userController.ts
+++ src/api/user/userController.ts
@@ -9,14 +9,18 @@ export const signup = async (req: Request, res: Response): Promise<any> => {
     try {
         const { name, email, password } = req.body;
 
+
         const parseData=await userSignUpSchema.safeParse(req.body);
+
         if(!parseData.success){
             return res.status(400).json({message:parseData.error});
         }
 
+
         const existingUser = await prisma.user.findUnique({
-            where: { email }
+            where: { email:email}
         });
+
 
         if (existingUser) {
             return res.status(400).json({ message: "User already exists" });
@@ -31,8 +35,9 @@ export const signup = async (req: Request, res: Response): Promise<any> => {
                 password: hashPassword
             }
         });
+        const token=jwt.sign({email},JWT_SECRET,{expiresIn:"1hr"});
 
-        return res.status(201).json({ message: "User created successfully" });
+        return res.status(201).json({ message: "User created successfully",token });
     } catch (error) {
         return res.status(500).json({ message: "Internal Server Error" });
     }
